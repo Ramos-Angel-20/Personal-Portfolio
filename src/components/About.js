c3c5515f04731c3d0f6f4677320ef18708d06308
Ramos-Angel-20 <miguelan47@yahoo.com.mx>
2021-10-20 12:28:49 -0700

    Se mejoro el mensaje del about

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,29 @@
+import devSvg from '../assets/dev.svg';
+
+
 const About = () => {
     return (
-        <div>
+        <div className='about container'>
+
+            <div className='about__img'>
+                <img src={devSvg} alt='Sobre Mi'></img>
+            </div>
+
+            <div className='about__content'>
+
+                <h1>Sobre Mi</h1>
+
+                <p>
+                    Soy un estudiante del Instituto Tecnológico de Tijuana (ITT) de la carrera de ingeniería
+                    informática, actualmente curso el octavo semestre. <br/>
+                    Estoy en busca de mi primer trabajo o de la liberación de mi servicio social y 
+                    mis practicas profesionales en alguna empresa que este desarrollando un producto o servicio
+                    de software. <br/>
+                    Mi inteción es formarme como programador web ya que todo ese mundo me parece apasionante, desde
+                    el frontend hasta el backend, por tal motivo pongo ofrezco mi tiempo y mis habilidades para el 
+                    desarrollo de software.
+                </p>
+            </div>
         </div>
     );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            &copy; <p>Angel Ramos</p>
+        </div>
+    );
+}
+
+export default Footer;
